fd704a0009f8c4b9338e5bdab00f6e514acf67bb
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-07-08 23:46:07 +0200

    controller

--- routes/post.js
+++ routes/post.js
@@ -1,3 +1,8 @@
-exports.getPosts = (req, res) => {
-    res.send("puncika");
-}
+const express = require('express');
+const postController = require('../controllers/postController');
+
+const router = express.Router();
+
+router.get('/', postController.getPosts);
+
+module.exports = router;
